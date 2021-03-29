96ad40887112343440c8278e28c69ecd44015e16
Eugene B <diemorgenstern@gmail.com>
2021-03-28 22:52:02 -0400

    feat: finish createDreamTeam funcion

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(membersArr) {
-  
+module.exports = function createDreamTeam(membersArr=[]) {
+  if (!(Array.isArray(membersArr) && (membersArr.length > 0))) return false;
+  return membersArr.filter(el => (typeof(el) === 'string')).map(el => el.trim().charAt(0).toUpperCase()).sort().join('');
 };
