23c847c27e2beb1b5fcf5247cf45262557439ed7
Eugene B <diemorgenstern@gmail.com>
2021-03-28 23:46:10 -0400

    feat: finished extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|',
+  } = options;
+
+  if (str === null) str = 'null';
+  if (options.addition === null) addition = 'null';
+
+  let repeatArr = [];
+  for (let i = 0; i < additionRepeatTimes; i++){
+    repeatArr.push(addition)
+  };
+  let repeatedSeparator = repeatArr.join(additionSeparator);
+
+  stringArr = [];
+  for (let i = 0; i < repeatTimes; i++){
+    stringArr.push(str + repeatedSeparator);
+  };
+  let repeatedStr = stringArr.join(separator);
+
+  return repeatedStr;
 };
-  
