b3050d57139e0943873e623d6cf0844fa5bda583
dermorgenstern <diemorgenstern@gmail.com>
2021-03-29 14:01:30 -0400

    feat: finish transform

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr = '') {
+  
+  if (!(arr instanceof Array)) throw new Error('Not implemented');
+  
+  let sArr = arr;
+
+  returnedArr = [];
+  
+  for(let i = 0; i < sArr.length; i++){
+    
+    if (sArr[i] === '--discard-next') {
+      if (sArr.length > i + 1 ) {
+        i = i + 1;
+        returnedArr.push(undefined);
+      }
+    } else if (sArr[i] === '--discard-prev') { 
+      if (returnedArr.length > 0) returnedArr.pop();
+    } else if (sArr[i] === '--double-next') { 
+      if ((sArr.length > i + 1) && true) returnedArr.push(sArr[i + 1]);
+    } else if (sArr[i] === '--double-prev') {
+      returnedArr.push(returnedArr[returnedArr.length - 1]);
+    } else {
+      returnedArr.push(sArr[i]);
+    }
+    
+  }
+
+  return returnedArr.filter(el => ( typeof(el) !== 'undefined' ) );
+
 };
